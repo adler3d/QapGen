f58b877007dd5d25d707417c52e546fdda528d36
adler3d <adler3d@gmail.com>
2025-06-25 11:50:40 +0300

    t_json to QapDSL v2

--- src/QapGenV2025/Release/t_json.qapdsl.hpp
+++ src/QapGenV2025/Release/t_json.qapdsl.hpp
@@ -1,160 +1,97 @@
 t_json{
 
-t_true=>i_value{{go_const("true");}}
-t_false=>i_value{{go_const("false");}}
-t_null=>i_value{{go_const("null");}}
+t_true=>i_value{"true"}
+t_false=>i_value{"false"}
+t_null=>i_value{"null"}
 
 t_string=>i_value{
-  t_raw=>i_item{string body;{go_any(body,dip_inv("\"\\\n"));}}
-  t_fix=>i_item{char body='z';{go_const("\\");go_any_char(body,"\"\\/bfnrt");}}
-  t_hex=>i_item{array(char,4) arr;{go_const("\\u");go_any_arr_char(arr,gen_dips("09afAF"));}}
-  t_items{vector<TAutoPtr<i_item>> arr;{go_auto(arr);}}
+  t_raw=>i_item{string body=any(dip_inv("\"\\\n"));}
+  t_fix=>i_item{"\\" char body=any_char("\"\\/bfnrt");}
+  t_hex=>i_item{"\\u" array(char,4) arr=any_arr_char(gen_dips("09afAF"));}
+  t_items{vector<TAutoPtr<i_item>> arr;}
+  "\""
   t_items body;
-  {
-    go_const("\"");
-    go_auto(body);
-    go_const("\"");
-  }
+  "\""
 }
 
 t_number=>i_value{
-  t_minus{{go_const("-");}}
+  t_minus{" - "}
   t_frac{
-    string arr;
-    {
-      M+=go_const(".");
-      M+=go_any(arr,gen_dips("09"));
-    }
+    "."
+    string arrany(gen_dips("09"));
   }
   t_sign{
-    char sign='+';
-    {
-      go_any_char(sign,"-+");
-    }
+    char sign=any_char("-+");
   }
   t_exp{
-    char e='e';
-    TAutoPtr<t_sign> sign;
-    string arr;
-    {
-      M+=go_any_char(e,"eE");
-      O+=go_auto(sign);
-      M+=go_any(arr,gen_dips("09"));
-    }
+    char e=any_char("eE");
+    TAutoPtr<t_sign> sign?;
+    string arr=any(gen_dips("09");
   }
   t_num=>i_int{
-    char first='1';
-    string num;
-    {
-      M+=go_any_char(first,gen_dips("19"));
-      O+=go_any(num,gen_dips("09"));
-    }
+    char first=any_char(gen_dips("19"));
+    string num=any(gen_dips("09"))?;
   }
-  t_zero=>i_int{{go_const("0");}}
-  TAutoPtr<t_minus> minus;
+  t_zero=>i_int{"0"}
+  TAutoPtr<t_minus> minus?;
   TAutoPtr<i_int> val;
-  TAutoPtr<t_frac> frac;
-  TAutoPtr<t_exp> exp;
-  {
-    O+=go_auto(minus);
-    M+=go_auto(val);
-    O+=go_auto(frac);
-    O+=go_auto(exp);
-  }
+  TAutoPtr<t_frac> frac?;
+  TAutoPtr<t_exp> exp?;
 }
 
 t_sep{
-  string body;
-  {
-    go_any(body," \t\r\n");
-  }
+  string body=any(" \t\r\n");
 }
 
 t_empty_array=>i_value{
-  t_sep sep;
-  {
-    M+=go_const("[");
-    O+=go_auto(sep);
-    M+=go_const("]");
-  }
+  "[" 
+  t_sep sep?;
+  "]"
 }
 
 t_value{
-  t_sep sep;
+  t_sep sep?;
   TAutoPtr<i_value> body;
-  {
-    O+=go_auto(sep);
-    M+=go_auto(body);
-  }
 }
 
 t_comma_value{
-  t_sep sep;
+  t_sep sep?;
+  ","
   t_value body;
-  {
-    O+=go_auto(sep);
-    M+=go_const(",");
-    M+=go_auto(body);
-  }
 }
-
 t_array=>i_value{
+  "["
   t_value first;
-  vector<t_comma_value> arr;
-  t_sep sep;
-  {
-    M+=go_const("[");
-    M+=go_auto(first);
-    O+=go_auto(arr);
-    O+=go_auto(sep);
-    M+=go_const("]");
-  }
+  vector<t_comma_value> arr?;
+  t_sep sep?;
+  "]"
 }
 
 t_empty_object=>i_value{
-  t_sep sep;
-  {
-    M+=go_const("{");
-    O+=go_auto(sep);
-    M+=go_const("}");
-  }
+  "{"
+  t_sep sep?;
+  "}"
 }
 
 t_pair{
-  t_sep sep0;
+  t_sep sep0?;
   t_string key;
-  t_sep sep1;
+  t_sep sep1?;
+  ":"
   t_value value;
-  {
-    O+=go_auto(sep0);
-    M+=go_auto(key);
-    O+=go_auto(sep1);
-    M+=go_const(":");
-    M+=go_auto(value);
-  }
 }
 
 t_comma_pair{
-  t_sep sep;
+  t_sep sep?;
+  ","
   t_pair body;
-  {
-    O+=go_auto(sep);
-    M+=go_const(",");
-    M+=go_auto(body);
-  }
 }
 
 t_object=>i_value{
+  "{"
   t_pair first;
   vector<t_comma_pair> arr;
-  t_sep sep;
-  {
-    M+=go_const("{");
-    M+=go_auto(first);
-    O+=go_auto(arr);
-    O+=go_auto(sep);
-    M+=go_const("}");
-  }
+  t_sep sep?;
+  "}"
 }
-
 }
