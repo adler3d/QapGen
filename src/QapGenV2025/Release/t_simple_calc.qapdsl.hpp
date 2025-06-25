b383cc417f50c20047099c9719e0bab6194601bd
adler3d <adler3d@gmail.com>
2025-06-25 15:07:02 +0300

    upd

--- src/QapGenV2025/Release/t_simple_calc.qapdsl.hpp
+++ src/QapGenV2025/Release/t_simple_calc.qapdsl.hpp
@@ -1,73 +1,38 @@
 t_simple_calc{
   t_term{
     TAutoPtr<i_term> value;
-    {
-      go_auto(value);
-    }
   }
   t_number=>i_term{
     t_ext{
-      string v;
-      {
-        go_const(".");
-        go_any(v,gen_dips("09"));
-      }
+      "."
+      string v=any(gen_dips("09"));
     }
     t_impl{
-      string bef;
-      TAutoPtr<t_ext> ext;
-      {
-        M+=go_any(bef,gen_dips("09"));
-        O+=go_auto(ext);
-      }
+      string bef=any(gen_dips("09"));
+      TAutoPtr<t_ext> ext?;
     }
-    string value;
-    {
-      go_str<t_impl>(value);
-    }
+    string value=str<t_impl>();
   }
   t_divmul{
     t_elem{
-      string oper;
+      string oper=any_str_from_vec(split("/,*",","));
       t_term expr;
-      {
-        go_any_str_from_vec(oper,split("/,*",","));
-        go_auto(expr);
-      }
     }
     t_term first;
-    vector<t_elem> arr;
-    {
-      M+=go_auto(first);
-      O+=go_auto(arr);
-    }
+    vector<t_elem> arr?;
   }
   t_addsub{
     t_elem{
-      string oper;
+      string oper=any_str_from_vec(split("+,-",","));
       t_divmul expr;
-      {
-        go_any_str_from_vec(oper,split("+,-",","));
-        go_auto(expr);
-      }
     }
     t_divmul first;
-    vector<t_elem> arr;
-    {
-      M+=go_auto(first);
-      O+=go_auto(arr);
-    }
+    vector<t_elem> arr?;
   }
   t_scope=>i_term{
+    "("
     t_addsub value;
-    {
-      go_const("(");
-      go_auto(value);
-      go_const(")");
-    }
+    ")"
   }
   t_addsub value;
-  {
-    go_auto(value);
-  }
 }
