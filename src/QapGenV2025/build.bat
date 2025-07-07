313528d3288c8c79953ea445e0c43d261db0eb93
adler3d <adler3d@gmail.com>
2025-07-07 18:08:17 +0300

    upd

---
+++ src/QapGenV2025/build.bat
@@ -0,0 +1,6 @@
+cl.exe /O2 /std:c++17 /bigobj /EHsc QapGen/QapGen.cpp
+link.exe "/SUBSYSTEM:WINDOWS" "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" QapGen.obj
+move QapGen.exe Release
+cd Release
+QapGen.exe input.qapdsl.hpp
+cd ..
---
+++ src/QapGenV2025/build.sh
@@ -0,0 +1,5 @@
+g++ -O2 -std=c++17 QapGen/QapGen.cpp -o QapGen.elf
+mv QapGen.elf Release
+cd Release
+./QapGen.elf input.qapdsl.hpp
+cd ..
