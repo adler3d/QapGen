75af71cc08af6a2c47a04ac5fdc79a252890f15e
adler3d <adler3d@gmail.com>
2025-07-08 16:15:47 +0300

    upd

--- stable/build.bat
+++ stable/build.bat
@@ -1,7 +1,22 @@
+setlocal enabledelayedexpansion
+rem ¬ключаем прерывание при ошибке
+set ERRORLEVEL=0
 cl.exe /std:c++17 /bigobj /EHsc /DQAP_DEBUG /Zi /Od /DEBUG /Fd"QapGen.pdb" QapGen/QapGen.cpp
+if errorlevel 1 (
+  echo err, stop working
+  exit /b %errorlevel%
+)
 link.exe /DEBUG "/SUBSYSTEM:WINDOWS" "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" QapGen.obj
+if errorlevel 1 (
+  echo err, stop working
+  exit /b %errorlevel%
+)
 move QapGen.exe Release
 cd Release
 QapGen.exe input.qapdsl.hpp
+if errorlevel 1 (
+  echo err, stop working
+  exit /b %errorlevel%
+)
 dir
 cd ..
--- stable/build.sh
+++ stable/build.sh
@@ -1,3 +1,5 @@
+set -o errexit
+set -o pipefail
 g++ -O2 -std=c++17 QapGen/QapGen.cpp -o QapGen.elf
 mv QapGen.elf Release
 cd Release
