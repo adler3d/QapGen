63ddf6fd532a9769800b262e3647ec0c828fad1b
adler3d <adler3d@gmail.com>
2025-07-08 11:18:16 +0300

    upd

--- stable/build.bat
+++ stable/build.bat
@@ -1,4 +1,4 @@
-cl.exe /O2 /std:c++17 /bigobj /EHsc QapGen/QapGen.cpp
+cl.exe /O2 /std:c++17 /bigobj /EHsc /DQAP_DEBUG QapGen/QapGen.cpp
 link.exe "/SUBSYSTEM:WINDOWS" "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" QapGen.obj
 move QapGen.exe Release
 cd Release
