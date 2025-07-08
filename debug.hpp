b9b4805aeb974adf42b70332157d99e9a3382596
adler3d <adler3d@gmail.com>
2025-07-08 07:58:35 +0300

    upd

---
+++ debug.hpp
@@ -0,0 +1,38 @@
+lexer:t_number
+lexer_after Do(r.body):t_number
+Do(*v.pfs) :t_number
+bef r.body&&!is_interface :t_number
+bef t_target_struct::t_body_impl::UberCast :t_number
+aft t_target_struct::t_body_impl::UberCast :t_number
+aft for(int iter=1;iter;iter--) :t_number
+aft this->dev.get_sep_lex(pc->value); :t_number
+aft Do for Do :t_number
+D+=go_any(body,gen_dips("09"));
+bef load_obj_full(sc,cmd); :t_number
+bef mode
+bef go_str::go_auto
+aft go_str::go_auto
+bef go_str::go_auto
+aft go_str::go_auto
+bef func
+bef go_str::go_auto
+aft go_str::go_auto
+bef go_str::go_blob
+bef vector<string> pcs=split(*pcm... :t_number
+aft body+=get_temp... :t_number
+aft r.body&&!is_interface :t_number
+bef need_attrs :t_number
+bef before_head+out+body :t_number
+typedef:
+typedef_after_save_obj:
+typedef:
+typedef_after_save_obj:
+lexer:t_str_item_raw
+lexer_after Do(r.body):t_str_item_raw
+lexer:t_str_item_hex
+lexer_after Do(r.body):t_str_item_hex
+lexer:t_str_item_num
+lexer_after Do(r.body):t_str_item_num
+lexer:t_str_item_fix
+lexer_after Do(r.body):t_str_item_fix
+lexer:t_str_item
