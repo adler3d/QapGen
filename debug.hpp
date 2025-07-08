a491bef5214ee6d57ba35969513028070527b46b
adler3d <adler3d@gmail.com>
2025-07-08 08:08:01 +0300

    upd

--- debug.hpp
+++ debug.hpp
@@ -1,253 +1,139 @@
 
-2025-07-08T04:45:28.6310022Z {"parse_ms":6.473}
-2025-07-08T04:45:28.6310710Z {"need_igrab done in ":0.043}
-2025-07-08T04:45:28.6311594Z {"need_grab done in ":0.009}
-2025-07-08T04:45:28.6312103Z lexer:i_str_item
-2025-07-08T04:45:28.6312572Z lexer_after Do(r.body):i_str_item
-2025-07-08T04:45:28.6313566Z lexer:i_char_item
-2025-07-08T04:45:28.6314022Z lexer_after Do(r.body):i_char_item
-2025-07-08T04:45:28.6314505Z lexer:i_sep
-2025-07-08T04:45:28.6314903Z lexer_after Do(r.body):i_sep
-2025-07-08T04:45:28.6315393Z lexer:i_simple_expr
-2025-07-08T04:45:28.6315870Z lexer_after Do(r.body):i_simple_expr
-2025-07-08T04:45:28.6316462Z {"interface_autogen done in ":0.325}
-2025-07-08T04:45:28.6318113Z lexer:t_parent_fullname_reslove_algo_test_20250701
-2025-07-08T04:45:28.6318797Z lexer:t_other
-2025-07-08T04:45:28.6319258Z lexer_after Do(r.body):t_other
-2025-07-08T04:45:28.6320613Z lexer_after Do(r.body):t_parent_fullname_reslove_algo_test_20250701
-2025-07-08T04:45:28.6321249Z lexer:t_using_test_20250628
-2025-07-08T04:45:28.6321692Z lexer:t_sep
-2025-07-08T04:45:28.6322096Z lexer_after Do(r.body):t_sep
-2025-07-08T04:45:28.6322529Z lexer:t_foo
-2025-07-08T04:45:28.6323002Z lexer:t_bar
-2025-07-08T04:45:28.6323353Z lexer:t_baz
-2025-07-08T04:45:28.6323724Z lexer_after Do(r.body):t_baz
-2025-07-08T04:45:28.6324179Z lexer_after Do(r.body):t_bar
-2025-07-08T04:45:28.6324630Z lexer_after Do(r.body):t_foo
-2025-07-08T04:45:28.6325138Z lexer_after Do(r.body):t_using_test_20250628
-2025-07-08T04:45:28.6325666Z lexer:t_number
-2025-07-08T04:45:28.6326054Z lexer_after Do(r.body):t_number
-2025-07-08T04:45:28.6326530Z Do(*v.pfs) :t_number
-2025-07-08T04:45:28.6326988Z bef r.body&&!is_interface :t_number
-2025-07-08T04:45:28.6332071Z bef t_target_struct::t_body_impl::UberCast :t_number
-2025-07-08T04:45:28.6332532Z aft t_target_struct::t_body_impl::UberCast :t_number
-2025-07-08T04:45:28.6332871Z aft for(int iter=1;iter;iter--) :t_number
-2025-07-08T04:45:28.6333197Z aft this->dev.get_sep_lex(pc->value); :t_number
-2025-07-08T04:45:28.6333501Z aft Do for Do :t_number
-2025-07-08T04:45:28.6333743Z D+=go_any(body,gen_dips("09"));
-2025-07-08T04:45:28.6334005Z bef load_obj_full(sc,cmd); :t_number
-2025-07-08T04:45:28.6334254Z bef mode
-2025-07-08T04:45:28.6334463Z bef go_str::go_auto
-2025-07-08T04:45:28.6334671Z aft go_str::go_auto
-2025-07-08T04:45:28.6334879Z bef go_str::go_auto
-2025-07-08T04:45:28.6335080Z aft go_str::go_auto
-2025-07-08T04:45:28.6335278Z bef func
-2025-07-08T04:45:28.6335465Z bef go_str::go_auto
-2025-07-08T04:45:28.6335669Z aft go_str::go_auto
-2025-07-08T04:45:28.6335880Z bef go_str::go_blob
-2025-07-08T04:45:28.6336084Z aft go_str::go_blob
-2025-07-08T04:45:28.6336277Z bef $sep2
-2025-07-08T04:45:28.6336580Z bef go_str::go_auto
-2025-07-08T04:45:28.6336786Z aft go_str::go_auto
-2025-07-08T04:45:28.6337042Z bef templ_params
-2025-07-08T04:45:28.6337430Z bef go_str::go_auto
-2025-07-08T04:45:28.6337789Z aft go_str::go_auto
-2025-07-08T04:45:28.6338118Z bef (
-2025-07-08T04:45:28.6338438Z bef params
-2025-07-08T04:45:28.6338776Z bef go_auto
-2025-07-08T04:45:28.6339147Z bef t_cmd_param::go_str<t_impl>
-2025-07-08T04:45:28.6339725Z bef go_str::go_auto
-2025-07-08T04:45:28.6340122Z bef t_cmd_param::t_impl::go_vec+
-2025-07-08T04:45:28.6340527Z bef go_auto
-2025-07-08T04:45:28.6340859Z bef go_str::go_auto
-2025-07-08T04:45:28.6341212Z aft go_str::go_auto
-2025-07-08T04:45:28.6341527Z bef go_str::go_blob
-2025-07-08T04:45:28.6341875Z aft go_str::go_blob
-2025-07-08T04:45:28.6342235Z aft t_expr_call::func
-2025-07-08T04:45:28.6342616Z bef go_str::go_auto
-2025-07-08T04:45:28.6342952Z bef self
-2025-07-08T04:45:28.6343250Z aft self
-2025-07-08T04:45:28.6343580Z bef go_str::go_auto
-2025-07-08T04:45:28.6343925Z aft go_str::go_auto
-2025-07-08T04:45:28.6344276Z bef go_str::go_blob
-2025-07-08T04:45:28.6344626Z aft go_str::go_blob
-2025-07-08T04:45:28.6345240Z aft t_impl::go_auto
-2025-07-08T04:45:28.6345627Z aft t_impl::ok
-2025-07-08T04:45:28.6345971Z aft go_str::go_auto
-2025-07-08T04:45:28.6346331Z bef go_str::go_blob
-2025-07-08T04:45:28.6346678Z aft go_str::go_blob
-2025-07-08T04:45:28.6347036Z aft go_str<t_impl>
-2025-07-08T04:45:28.6347381Z t_expr_var::ok
-2025-07-08T04:45:28.6347709Z aft go_auto
-2025-07-08T04:45:28.6348029Z aft go_vec::break
-2025-07-08T04:45:28.6348406Z aft t_cmd_param::t_impl::go_vec+
-2025-07-08T04:45:28.6348841Z aft t_cmd_param::t_impl::ok
-2025-07-08T04:45:28.6349238Z aft go_str::go_auto
-2025-07-08T04:45:28.6349739Z bef go_str::go_blob
-2025-07-08T04:45:28.6350096Z aft go_str::go_blob
-2025-07-08T04:45:28.6350471Z aft t_cmd_param::go_str<t_impl>
-2025-07-08T04:45:28.6350864Z aft go_auto
-2025-07-08T04:45:28.6351190Z aft go_vec::break
-2025-07-08T04:45:28.6351521Z bef go_auto
-2025-07-08T04:45:28.6351868Z bef t_cmd_param::go_str<t_impl>
-2025-07-08T04:45:28.6352271Z bef go_str::go_auto
-2025-07-08T04:45:28.6352682Z bef t_cmd_param::t_impl::go_vec+
-2025-07-08T04:45:28.6353082Z bef go_auto
-2025-07-08T04:45:28.6353600Z bef go_str::go_auto
-2025-07-08T04:45:28.6353950Z aft go_str::go_auto
-2025-07-08T04:45:28.6354306Z bef go_str::go_blob
-2025-07-08T04:45:28.6354660Z aft go_str::go_blob
-2025-07-08T04:45:28.6355020Z aft t_expr_call::func
-2025-07-08T04:45:28.6355400Z bef t_expr_call::params
-2025-07-08T04:45:28.6355760Z bef go_auto
-2025-07-08T04:45:28.6356107Z bef t_cmd_param::go_str<t_impl>
-2025-07-08T04:45:28.6356518Z bef go_str::go_auto
-2025-07-08T04:45:28.6356891Z bef t_cmd_param::t_impl::go_vec+
-2025-07-08T04:45:28.6357285Z bef go_auto
-2025-07-08T04:45:28.6357608Z bef go_str<t_str_seq>
-2025-07-08T04:45:28.6357969Z bef go_str::go_auto
-2025-07-08T04:45:28.6358322Z aft go_str::go_auto
-2025-07-08T04:45:28.6358673Z bef go_str::go_blob
-2025-07-08T04:45:28.6359023Z aft go_str::go_blob
-2025-07-08T04:45:28.6359362Z aft go_str<t_str_seq>
-2025-07-08T04:45:28.6360007Z aft go_auto
-2025-07-08T04:45:28.6360341Z aft go_vec::break
-2025-07-08T04:45:28.6360719Z aft t_cmd_param::t_impl::go_vec+
-2025-07-08T04:45:28.6361160Z aft t_cmd_param::t_impl::ok
-2025-07-08T04:45:28.6361551Z aft go_str::go_auto
-2025-07-08T04:45:28.6361903Z bef go_str::go_blob
-2025-07-08T04:45:28.6362243Z aft go_str::go_blob
-2025-07-08T04:45:28.6362611Z aft t_cmd_param::go_str<t_impl>
-2025-07-08T04:45:28.6363006Z aft go_auto
-2025-07-08T04:45:28.6363332Z aft go_vec::break
-2025-07-08T04:45:28.6363686Z aft t_expr_call::params
-2025-07-08T04:45:28.6364069Z aft t_expr_call::ok
-2025-07-08T04:45:28.6364414Z bef go_str::go_auto
-2025-07-08T04:45:28.6364756Z bef self
-2025-07-08T04:45:28.6365069Z aft self
-2025-07-08T04:45:28.6365390Z bef go_str::go_auto
-2025-07-08T04:45:28.6365740Z aft go_str::go_auto
-2025-07-08T04:45:28.6366086Z bef go_str::go_blob
-2025-07-08T04:45:28.6366432Z aft go_str::go_blob
-2025-07-08T04:45:28.6366774Z aft t_impl::go_auto
-2025-07-08T04:45:28.6367135Z aft t_impl::ok
-2025-07-08T04:45:28.6367473Z aft go_str::go_auto
-2025-07-08T04:45:28.6367820Z bef go_str::go_blob
-2025-07-08T04:45:28.6368166Z aft go_str::go_blob
-2025-07-08T04:45:28.6368517Z aft go_str<t_impl>
-2025-07-08T04:45:28.6368857Z t_expr_var::ok
-2025-07-08T04:45:28.6369187Z aft go_auto
-2025-07-08T04:45:28.6369652Z aft go_vec::break
-2025-07-08T04:45:28.6370031Z aft t_cmd_param::t_impl::go_vec+
-2025-07-08T04:45:28.6370465Z aft t_cmd_param::t_impl::ok
-2025-07-08T04:45:28.6370856Z aft go_str::go_auto
-2025-07-08T04:45:28.6371207Z bef go_str::go_blob
-2025-07-08T04:45:28.6371551Z aft go_str::go_blob
-2025-07-08T04:45:28.6371918Z aft t_cmd_param::go_str<t_impl>
-2025-07-08T04:45:28.6372304Z aft go_auto
-2025-07-08T04:45:28.6372627Z aft go_vec::break
-2025-07-08T04:45:28.6372953Z bef )
-2025-07-08T04:45:28.6373273Z bef go_str::go_auto
-2025-07-08T04:45:28.6373619Z aft go_str::go_auto
-2025-07-08T04:45:28.6373953Z bef cmdso
-2025-07-08T04:45:28.6374270Z bef go_str::go_auto
-2025-07-08T04:45:28.6374626Z aft go_str::go_auto
-2025-07-08T04:45:28.6374984Z bef go_str::go_auto
-2025-07-08T04:45:28.6375330Z aft go_str::go_auto
-2025-07-08T04:45:28.6375662Z bef ;
-2025-07-08T04:45:28.6376146Z bef mode
-2025-07-08T04:45:28.6376591Z bef func
-2025-07-08T04:45:28.6376902Z bef $sep2
-2025-07-08T04:45:28.6377223Z bef templ_params
-2025-07-08T04:45:28.6377554Z bef (
-2025-07-08T04:45:28.6377852Z bef params
-2025-07-08T04:45:28.6378202Z bef t_cmd_param::go_str<t_impl>
-2025-07-08T04:45:28.6378627Z bef t_cmd_param::t_impl::go_vec+
-2025-07-08T04:45:28.6379031Z bef go_auto
-2025-07-08T04:45:28.6379351Z bef go_str::go_auto
-2025-07-08T04:45:28.6379859Z aft go_str::go_auto
-2025-07-08T04:45:28.6380214Z bef go_str::go_blob
-2025-07-08T04:45:28.6380556Z aft go_str::go_blob
-2025-07-08T04:45:28.6380915Z aft t_expr_call::func
-2025-07-08T04:45:28.6381275Z bef go_str::go_auto
-2025-07-08T04:45:28.6381611Z bef self
-2025-07-08T04:45:28.6381908Z aft self
-2025-07-08T04:45:28.6382229Z bef go_str::go_auto
-2025-07-08T04:45:28.6382572Z aft go_str::go_auto
-2025-07-08T04:45:28.6382921Z bef go_str::go_blob
-2025-07-08T04:45:28.6383264Z aft go_str::go_blob
-2025-07-08T04:45:28.6383615Z aft t_impl::go_auto
-2025-07-08T04:45:28.6383965Z aft t_impl::ok
-2025-07-08T04:45:28.6384486Z aft go_str::go_auto
-2025-07-08T04:45:28.6384837Z bef go_str::go_blob
-2025-07-08T04:45:28.6385182Z aft go_str::go_blob
-2025-07-08T04:45:28.6385528Z aft go_str<t_impl>
-2025-07-08T04:45:28.6385868Z t_expr_var::ok
-2025-07-08T04:45:28.6386198Z aft go_auto
-2025-07-08T04:45:28.6386518Z aft go_vec::break
-2025-07-08T04:45:28.6386888Z aft t_cmd_param::t_impl::go_vec+
-2025-07-08T04:45:28.6387329Z aft t_cmd_param::t_impl::ok
-2025-07-08T04:45:28.6387763Z aft t_cmd_param::go_str<t_impl>
-2025-07-08T04:45:28.6388195Z bef t_cmd_param::go_str<t_impl>
-2025-07-08T04:45:28.6388636Z bef t_cmd_param::t_impl::go_vec+
-2025-07-08T04:45:28.6389033Z bef go_auto
-2025-07-08T04:45:28.6389371Z bef go_str::go_auto
-2025-07-08T04:45:28.6389898Z aft go_str::go_auto
-2025-07-08T04:45:28.6390261Z bef go_str::go_blob
-2025-07-08T04:45:28.6390612Z aft go_str::go_blob
-2025-07-08T04:45:28.6390977Z aft t_expr_call::func
-2025-07-08T04:45:28.6391358Z bef t_expr_call::params
-2025-07-08T04:45:28.6391736Z bef go_auto
-2025-07-08T04:45:28.6392099Z bef t_cmd_param::go_str<t_impl>
-2025-07-08T04:45:28.6392513Z bef go_str::go_auto
-2025-07-08T04:45:28.6392892Z bef t_cmd_param::t_impl::go_vec+
-2025-07-08T04:45:28.6393297Z bef go_auto
-2025-07-08T04:45:28.6393622Z bef go_str<t_str_seq>
-2025-07-08T04:45:28.6394009Z bef go_str::go_auto
-2025-07-08T04:45:28.6394368Z aft go_str::go_auto
-2025-07-08T04:45:28.6394723Z bef go_str::go_blob
-2025-07-08T04:45:28.6395078Z aft go_str::go_blob
-2025-07-08T04:45:28.6395424Z aft go_str<t_str_seq>
-2025-07-08T04:45:28.6395778Z aft go_auto
-2025-07-08T04:45:28.6396104Z aft go_vec::break
-2025-07-08T04:45:28.6396487Z aft t_cmd_param::t_impl::go_vec+
-2025-07-08T04:45:28.6396922Z aft t_cmd_param::t_impl::ok
-2025-07-08T04:45:28.6397330Z aft go_str::go_auto
-2025-07-08T04:45:28.6397684Z bef go_str::go_blob
-2025-07-08T04:45:28.6398049Z aft go_str::go_blob
-2025-07-08T04:45:28.6398427Z aft t_cmd_param::go_str<t_impl>
-2025-07-08T04:45:28.6398825Z aft go_auto
-2025-07-08T04:45:28.6399159Z aft go_vec::break
-2025-07-08T04:45:28.6399691Z aft t_expr_call::params
-2025-07-08T04:45:28.6400100Z aft t_expr_call::ok
-2025-07-08T04:45:28.6400458Z bef go_str::go_auto
-2025-07-08T04:45:28.6400796Z bef self
-2025-07-08T04:45:28.6401099Z aft self
-2025-07-08T04:45:28.6401305Z bef go_str::go_auto
-2025-07-08T04:45:28.6401513Z aft go_str::go_auto
-2025-07-08T04:45:28.6401724Z bef go_str::go_blob
-2025-07-08T04:45:28.6401927Z aft go_str::go_blob
-2025-07-08T04:45:28.6402137Z aft t_impl::go_auto
-2025-07-08T04:45:28.6402344Z aft t_impl::ok
-2025-07-08T04:45:28.6402562Z aft go_str::go_auto
-2025-07-08T04:45:28.6402769Z bef go_str::go_blob
-2025-07-08T04:45:28.6402978Z aft go_str::go_blob
-2025-07-08T04:45:28.6403191Z aft go_str<t_impl>
-2025-07-08T04:45:28.6403399Z t_expr_var::ok
-2025-07-08T04:45:28.6403602Z aft go_auto
-2025-07-08T04:45:28.6403800Z aft go_vec::break
-2025-07-08T04:45:28.6404033Z aft t_cmd_param::t_impl::go_vec+
-2025-07-08T04:45:28.6404432Z aft t_cmd_param::t_impl::ok
-2025-07-08T04:45:28.6405032Z aft t_cmd_param::go_str<t_impl>
-2025-07-08T04:45:28.6405394Z bef )
-2025-07-08T04:45:28.6405728Z bef cmdso
-2025-07-08T04:45:28.6406033Z bef ;
-2025-07-08T04:45:28.6406380Z aft load_obj_full(sc,cmd); :t_number
-2025-07-08T04:45:28.6406787Z bef Do(sc); :t_number
-2025-07-08T04:45:28.6407181Z aft Do(sc); :t_number
-2025-07-08T04:45:28.6407618Z bef vector<string> pcs=split(*pcm... :t_number
-2025-07-08T04:45:28.6408134Z aft body+=get_temp... :t_number
-2025-07-08T04:45:28.6408579Z aft r.body&&!is_interface :t_number
-2025-07-08T04:45:28.6409019Z bef need_attrs :t_number
-2025-07-08T04:45:28.6409445Z bef before_head+out+body :t_number
-2025-07-08T04:45:28.6410050Z typedef:
+{"parse_ms":10.1125}
+{"need_igrab done in ":0.1532}
+{"need_grab done in ":0.0294}
+lexer:i_str_item
+lexer_after Do(r.body):i_str_item
+lexer:i_char_item
+lexer_after Do(r.body):i_char_item
+lexer:i_sep
+lexer_after Do(r.body):i_sep
+lexer:i_simple_expr
+lexer_after Do(r.body):i_simple_expr
+{"interface_autogen done in ":0.6519}
+lexer:t_parent_fullname_reslove_algo_test_20250701
+lexer:t_other
+lexer_after Do(r.body):t_other
+lexer_after Do(r.body):t_parent_fullname_reslove_algo_test_20250701
+lexer:t_using_test_20250628
+lexer:t_sep
+lexer_after Do(r.body):t_sep
+lexer:t_foo
+lexer:t_bar
+lexer:t_baz
+lexer_after Do(r.body):t_baz
+lexer_after Do(r.body):t_bar
+lexer_after Do(r.body):t_foo
+lexer_after Do(r.body):t_using_test_20250628
+lexer:t_number
+lexer_after Do(r.body):t_number
+Do(*v.pfs) :t_number
+bef r.body&&!is_interface :t_number
+bef t_target_struct::t_body_impl::UberCast :t_number
+aft t_target_struct::t_body_impl::UberCast :t_number
+aft for(int iter=1;iter;iter--) :t_number
+aft this->dev.get_sep_lex(pc->value); :t_number
+aft Do for Do :t_number
+D+=go_any(body,gen_dips("09"));
+bef load_obj_full(sc,cmd); :t_number
+bef mode
+bef go_str::go_auto
+aft go_str::go_auto
+bef go_str::go_auto
+aft go_str::go_auto
+bef func
+bef go_str::go_auto
+aft go_str::go_auto
+bef go_str::go_blob
+aft go_str::go_blob
+bef $sep2
+bef go_str::go_auto
+aft go_str::go_auto
+bef templ_params
+bef go_str::go_auto
+aft go_str::go_auto
+bef (
+bef params
+bef go_auto
+bef t_cmd_param::go_str<t_impl>
+bef go_str::go_auto
+bef t_cmd_param::t_impl::go_vec+
+bef go_auto
+bef go_str::go_auto
+aft go_str::go_auto
+bef go_str::go_blob
+aft go_str::go_blob
+aft t_expr_call::func
+bef go_str::go_auto
+bef self
+aft self
+bef go_str::go_auto
+aft go_str::go_auto
+bef go_str::go_blob
+aft go_str::go_blob
+aft t_impl::go_auto
+aft t_impl::ok
+aft go_str::go_auto
+bef go_str::go_blob
+aft go_str::go_blob
+aft go_str<t_impl>
+t_expr_var::ok
+aft go_auto
+aft go_vec::break
+aft t_cmd_param::t_impl::go_vec+
+aft t_cmd_param::t_impl::ok
+aft go_str::go_auto
+bef go_str::go_blob
+aft go_str::go_blob
+aft t_cmd_param::go_str<t_impl>
+aft go_auto
+aft go_vec::break
+bef go_auto
+bef t_cmd_param::go_str<t_impl>
+bef go_str::go_auto
+bef t_cmd_param::t_impl::go_vec+
+bef go_auto
+bef go_str::go_auto
+aft go_str::go_auto
+bef go_str::go_blob
+aft go_str::go_blob
+aft t_expr_call::func
+bef t_expr_call::params
+bef go_auto
+bef t_cmd_param::go_str<t_impl>
+bef go_str::go_auto
+bef t_cmd_param::t_impl::go_vec+
+bef go_auto
+bef go_str<t_str_seq>
+bef go_str::go_auto
+aft go_str::go_auto
+bef go_str::go_blob
+aft go_str::go_blob
+aft go_str<t_str_seq>
+aft go_auto
+aft go_vec::break
+aft t_cmd_param::t_impl::go_vec+
+aft t_cmd_param::t_impl::ok
+aft go_str::go_auto
+bef go_str::go_blob
+aft go_str::go_blob
+aft t_cmd_param::go_str<t_impl>
+aft go_auto
+aft go_vec::break
+aft t_expr_call::params
+aft t_expr_call::ok
+bef go_str::go_auto
+bef self
+aft self
+bef go_str::go_auto
+aft go_str::go_auto
+bef go_str::go_blob
+aft go_str::go_blob
+aft t_impl::go_auto
+aft t_impl::ok
+aft go_str::go_auto
+bef go_str::go_blob
+aft go_str::go_blob
+aft go_str<t_impl>
+t_expr_var::ok
+##[error]The operation was canceled.
