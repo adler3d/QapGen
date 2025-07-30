4d53126065593d8f7a27a037d268039069b97cb6
adler3d <adler3d@gmail.com>
2025-07-30 08:44:52 +0300

    old_main fn fix

--- old_main/game_box.h
+++ old_main/game_box.h
@@ -2,7 +2,7 @@
   //__UserMsg(__NetDownload("http://185.92.223.117/MarketGameV4.html?qapgen&github","/"));
   auto fn="branch.h";//9:07 16:20
   fn="t_type_expr.inl";
-  fn="samples/input.qapdsl";
+  fn="../samples/input.qapdsl";
   auto code=__file_get_contents(fn);
   real bef_time=__microtime();
   string msg=__gen_lexem_v01(code);
